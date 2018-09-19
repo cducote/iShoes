2b96495f286c7f8adf3bca670f53c15577f5496a
Chris Ducote <chrisd@slappy.local>
2018-09-19 16:33:10 -0400

    schema complete

--- db/schema.js
+++ db/schema.js
@@ -1,12 +1,34 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
-const UserSchema = new Schema({
+const IncomeSchema = new Schema({
+    income = Number,
+    otherIncome = Number
+})
+
+const BillsSchema = new Schema({
+    rent = Number,
+    carInsurance = Number,
+    power = Number,
+    cc = Number,
+    other = Number
+}) 
+
+const ShoesieSchema = new Schema({
     username = String,
-    bills = [],
-    income = [],
+    bills = [BillsSchema],
+    income = [IncomeSchema],
     billsTotal = number,
     incomeTotal = number
     affordShoes = Boolean
 })
 
+const IncomeSchema = mongoose.model('Income', IncomeSchema)
+const BillsSchema = mongoose.model('Bills', BillsSchema)
+const ShoesieSchema = mongoose.model('User', ShoesieSchema)
+
+module.exports = {
+    Income: IncomeModel,
+    Bills: BillModel,
+    Shoesie: ShoesieModel
+}
