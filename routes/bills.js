e644e12ad9b2935ea1724e9f9ae4b90f5f4e625b
Chris Ducote <chrisd@slappy.local>
2018-09-20 18:48:57 -0400

    updates bills for now

--- routes/bills.js
+++ routes/bills.js
@@ -17,7 +17,27 @@ router.get('/', (req, res) => {
         console.log(error)
     })
 })
-
+// Edit Form
+router.get('/edit', (req, res) => {
+    Shoesie.findById(req.params.shoesieId)
+      .then((shoesie) => {
+        console.log(shoesie)
+        res.render('bills/edit', { shoesie,
+        shoesieId: req.params.shoesieId,
+        bills: shoesie.bills
+     })
+    })
+    .catch(error => {
+        console.log(error)
+    })
+})
+// Update billsies
+router.put('/', (req, res) => {
+    Shoesie.create(req.body)
+      .then((shoesie) => {
+        res.redirect(`/shoesies/${shoesie._id}/bills`)
+      })
+  })   
 
 
 
